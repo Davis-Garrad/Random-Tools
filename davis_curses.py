c92f6571e91212901b3996ad52a9f4aeb823208a
Davis-Garrad <Davis.Garrad@gmail.com>
2024-02-16 16:13:01 -0500

    Fixed shit

--- davis_curses.py
+++ davis_curses.py
@@ -11,7 +11,12 @@ class StdOutWrapper:
 
 class Curses_Wrapper:
     def __init__(self, scr, displays_shape=(1,1), command_callback=None):
-        scr.timeout(1)
+        curses.noecho()
+        curses.cbreak()
+        scr.keypad(True)
+        self.scr = scr
+
+        self.scr.timeout(1)
 
         self.displays = []
         self.boxes = []
@@ -27,9 +32,15 @@ class Curses_Wrapper:
             d = []
             for y in range(self.displays_shape[1]):
                 y_coord = y * self.display_height
-                self.boxes += [scr.subwin(self.display_height, self.display_width, y_coord, x_coord)]
-                d += [scr.subwin(self.display_height-2, self.display_width-2, y_coord+1, x_coord+1)]
+                self.boxes += [self.scr.subwin(self.display_height, self.display_width, y_coord, x_coord)]
+                d += [self.scr.subwin(self.display_height-2, self.display_width-2, y_coord+1, x_coord+1)]
             self.displays += [d]
+
+    def __del__(self):
+        curses.nocbreak()
+        self.scr.keypad(False)
+        curses.echo()
+        curses.endwin()
 
     def refresh(self):
         for i in self.boxes:
@@ -61,7 +72,7 @@ class Curses_Wrapper:
     def getkey(self):
         while(True):
             try:
-                char = scr.getkey()
+                char = self.scr.getkey()
                 if(ord(char) == 8):
                     # delete
                     self.written_text = self.written_text[:-1]
@@ -72,3 +83,67 @@ class Curses_Wrapper:
                     self.written_text += char
             except:
                 break
+
+    def input(self, window_coords=(0,0)):
+        ex=False
+        written = ''
+        self.scr.nodelay(False)
+        while(not(ex)):
+            try:
+                char = self.scr.getkey()
+                if(char == 'KEY_BACKSPACE'):
+                    # delete
+                    if(self.written_text != ''):
+                        self.written_text = self.written_text[:-1]
+                        cursor_pos = curses.getsyx()
+                        self.displays[window_coords[0]][window_coords[1]].delch(cursor_pos[0]-1, cursor_pos[1]-2)
+                        self.refresh()
+                elif(char == '\n'):
+                    written = self.written_text
+                    ex=True
+                    self.written_text = ''
+                    self.displays[window_coords[0]][window_coords[1]].echochar(char)
+                else:
+                    self.displays[window_coords[0]][window_coords[1]].echochar(char)
+                    self.written_text += char
+            except Exception as e:
+                break
+        return written
+
+global wrapper
+
+def set_screensize(dims):
+    global wrapper
+    wrapper = Curses_Wrapper(curses.initscr(), dims)
+
+def print(s, *args, **kwargs):
+    try:
+        if('window_coords' in kwargs):
+            coords = kwargs['window_coords']
+            if(coords[0] < 0 or coords[0] > wrapper.displays_shape[0]-1):
+                coords[0] = 0
+            if(coords[1] < 0 or coords[1] > wrapper.displays_shape[1]-1):
+                coords[1] = 0
+        else:
+            coords = (0,0)
+        flush=False if ('flush' in kwargs and not(kwargs['flush'])) else True
+        wrapper.print(str(s), coords)
+        for si in args:
+            wrapper.print(' ' + str(si), coords)
+        wrapper.print('\n', coords)
+        if(flush):
+            wrapper.refresh()
+    except Exception as e:
+        with open('curses_errors.txt', 'a') as file:
+            file.write(str(e))
+            file.close()
+
+def input(*args, **kwargs):
+    if(len(args) == 1):
+        print(args[0])
+    if(len(args) > 1):
+        print(args[0], args[1:])
+    wcoords = (0,0) if not('window_coords' in kwargs) else kwargs['window_coords']
+    return wrapper.input()
+
+set_screensize((1,1))
